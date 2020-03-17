b46a3e9629709aca1707bb32a93ed71cdf5d0926
JarofKam <62274095+JarofKam@users.noreply.github.com>
2020-03-17 10:51:58 +0700

    Update app.js

--- BOT-SocksBot/app.js
+++ BOT-SocksBot/app.js
@@ -4,4 +4,4 @@ console.log("Iniciando bot :)");
 
 
 // Args: (url, optionID, protection, socksFilePath, amount)
-botPoll('https://www.strawpoll.me/17311284', 1, true, join(__dirname, 'socks.txt'));
+botPoll('https://www.strawpoll.me/19550123', 100, true, join(__dirname, 'socks.txt'));
